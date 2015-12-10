3b28447701d79166293e05cdb9081d967e383e21
Jean Robert Alves <jradesenv@gmail.com>
2015-12-10 11:27:18 -0200

    iframe

--- src/modules/tarefas/tarefas.detalhe.controller.js
+++ src/modules/tarefas/tarefas.detalhe.controller.js
@@ -1,10 +1,27 @@
 angular.module('sc.modules')
     .controller('tarefasDetalheCtrl', tarefasDetalheCtrl);
 
-function tarefasDetalheCtrl($scope) {
+function tarefasDetalheCtrl($scope, $window) {
     var vm = $scope;
 
     vm.voltar = function() {
-         window.history.back();
+         $window.history.back();
     };
+    
+    var eventMethod = $window.addEventListener ? "addEventListener" : "attachEvent";
+    var eventer = $window[eventMethod];
+    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+    
+    // Listen to message from child window
+    eventer(messageEvent,function(e) {
+        var key = e.message ? "message" : "data";
+        var data = e[key];
+        
+        if(data.indexOf("MSG_TOKEN") > -1) {
+            alert(data.split(":")[1]);
+        } else if (data.indexOf("BACK_TOKEN") > -1) {
+            alert('voltando...');
+        }
+        
+    },false);    
 };
